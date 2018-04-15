0ca19c87f7dcfe5cdd148e6002952f7a817f3b31
Tobias Winner <t.winner@donders.ru.nl>
2018-04-15 13:47:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import numpy as np
 import lqg
 
+# full LQG
 system = lqg.LQG(5)  # initialize LQG system with time horizon T = 5
 system.define('ABCQRVWX', np.matrix(np.eye(2)))  # define matrices
+data = system.sample(10)  # simulate 10 system runs
+
+# no feedback
+system = lqg.LQG(5)  # initialize LQG system with time horizon T = 5
+system.define('ABQRVX', np.matrix(np.eye(2)))  # define matrices
+data = system.sample(10)  # simulate 10 system runs
+
+# no control
+system = lqg.LQG(5)  # initialize LQG system with time horizon T = 5
+system.define('ACVWX', np.matrix(np.eye(2)))  # define matrices
 data = system.sample(10)  # simulate 10 system runs
