58df1d885cd3bef4d2465bbd0f9e51b75b47b15d
Tobias Winner <t.winner@donders.ru.nl>
2018-04-15 13:47:21 +0200

    Update lqg.py

--- lqg.py
+++ lqg.py
@@ -13,7 +13,7 @@ def lqe(A, C, V, W, X0):
     for t in range(horizon):
         K.append(X0[t] * C[t].T * np.linalg.pinv(C[t] * X0[t] * C[t].T + W[t]))
         X1.append(X0[t] - K[t] * C[t] * X0[t])
-        if t < horizon-1:
+        if t < horizon - 1:
             X0.append(A[t] * X1[t] * A[t].T + V[t])
     return K, X0, X1
 
@@ -53,7 +53,7 @@ class LQG:
                     'P': None, 'Q': None, 'R': None,
                     'V': None, 'W': None,
                     'X0': None, 'X1': None}
-    
+
     def define(self, string, val):
         for char in string:
             if char in 'ABCRVW':
@@ -62,21 +62,32 @@ class LQG:
                 self.var['Q'] = tvar(val, self.horizon, -1)
             elif char == 'X':
                 self.var['X0'] = tvar(val, 1)
-    
+
     def kalman(self):
         self.var['K'], self.var['X0'], self.var['X1'] = \
             lqe(self.var['A'], self.var['C'], self.var['V'], self.var['W'], self.var['X0'])
-    
+
     def control(self):
         self.var['L'], self.var['P'] = \
             lqr(self.var['A'], self.var['B'], self.var['Q'], self.var['R'])
-    
+
     def sample(self, n=1, x0=None, x=None, u=None, v=None, w=None):
-        self.kalman()
-        self.control()
+        y = None
 
         a = np.shape(self.var['A'][0])
-        c = np.shape(self.var['C'][0])
+
+        if not self.var['C'] is None:
+            c = np.shape(self.var['C'][0])
+            self.kalman()
+            obs = True
+        else:
+            obs = False
+
+        if not self.var['B'] is None:
+            self.control()
+            ctrl = True
+        else:
+            ctrl = False
 
         if x0 is None:  # default zero
             x0 = [np.matrix(np.zeros((a[0], n)))]
@@ -93,7 +104,7 @@ class LQG:
             x = [x]
             e = np.linalg.pinv(self.var['X0'][0]) * (x[0] - x0[0])
 
-        if w is None:
+        if w is None and obs:
             w = [np.random.randn(c[0], n)]
             for t in range(self.horizon - 1):
                 w.append(np.random.randn(c[0], n))
@@ -102,19 +113,31 @@ class LQG:
             for t in range(self.horizon - 1):
                 v.append(np.random.randn(a[0], n))
 
-        y = [self.var['C'][0] * x[0] + self.var['W'][0] * w[0]]
-        x1 = [x0[0] + self.var['K'][0] * (y[0] - self.var['C'][0] * x0[0])]
-        if u is None:
+        if obs:
+            y = [self.var['C'][0] * x[0] + self.var['W'][0] * w[0]]
+            x1 = [x0[0] + self.var['K'][0] * (y[0] - self.var['C'][0] * x0[0])]
+        else:
+            x1 = [x0[0]]
+        if u is None and ctrl:
             u = [self.var['L'][0] * x1[0]]
-        for t in range(self.horizon-1):
-            x0.append(self.var['A'][t] * x1[t] + self.var['B'][t] * u[t])
-            x.append(self.var['A'][t] * x[t] + self.var['B'][t] * u[t] + self.var['V'][t] * v[t])
-            y.append(self.var['C'][t] * x[t] + self.var['W'][t] * w[t])
-            x1.append(x0[t] + self.var['K'][t] * (y[t] - self.var['C'][t] * x0[t]))
-            if len(u) <= t+1:
-                u.append(self.var['L'][t] * x1[t])
+        for t in range(self.horizon - 1):
+            if ctrl:
+                x0.append(self.var['A'][t] * x1[t] + self.var['B'][t] * u[t])
+                x.append(self.var['A'][t] * x[t] + self.var['B'][t] * u[t] + self.var['V'][t] * v[t])
+            else:
+                x0.append(self.var['A'][t] * x1[t])
+                x.append(self.var['A'][t] * x[t] + self.var['V'][t] * v[t])
+            if obs:
+                y.append(self.var['C'][t] * x[t] + self.var['W'][t] * w[t])
+                x1.append(x0[t] + self.var['K'][t] * (y[t] - self.var['C'][t] * x0[t]))
+            else:
+                x1.append(x0[t])
+            if ctrl:
+                if len(u) <= t + 1:
+                    u.append(self.var['L'][t] * x1[t])
 
         noise = {'x': e, 'v': v, 'w': w}
         kf = {'x0': x0, 'x1': x1}
         data = {'x': x, 'y': y, 'u': u, 'kf': kf, 'noise': noise, 'cost': {}}
+
         return data
